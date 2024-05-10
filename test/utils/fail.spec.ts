88660249bc7f61ebe75ba73494b13441c9cac3b9
kabeep <zzx2067@outlook.com>
2024-05-11 01:33:16 +0800

    test: update fail, warn cases

--- test/utils/fail.spec.ts
+++ test/utils/fail.spec.ts
@@ -4,7 +4,6 @@ import Fail from '../../src/utils/fail';
 test('Fail - toString() method should return the formatted fail message', () => {
     const fail = new Fail('This is a failing message.');
     const result = fail.toString();
-    const expected =
-        ' \u001b[38;2;17;15;24m\u001b[48;2;255;103;103m Fail \u001b[49m\u001b[39m This is a failing message.';
-    expect(result).toBe(expected);
+    expect(result).toContain('Fail');
+    expect(result).toContain('This is a failing message.');
 });
